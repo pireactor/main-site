57664d57f47e8942ee44969a00d571aa1e383c6d
unknown <ilia@e1off.dev>
2022-06-22 18:17:05 +0300

    bug form

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import solid from "@astrojs/solid-js";
-
 import astro from "astro-robots-txt";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(),],
   experimental: astro(),
+  adapter: netlify(),
 });
