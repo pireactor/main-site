eae33bb32d5e09e8c577c83ac17b689ca7e4d0a8
Daniel Shelepanov <d.shelepanov@postgrespro.ru>
2022-06-28 16:02:13 +0300

    testing

--- client/netlify/functions/contact/main.go
+++ client/netlify/functions/contact/main.go
@@ -1,17 +1,67 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"net/smtp"
+	"strconv"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+const responseOK string = `{"errcode":200}`
+const responseBadConfigFile string = `{"errcode":201}`
+const responseBadConfigContent string = `{"errcode":202}`
+const responseSMTPError string = `{"errcode":201}`
+
+type config struct {
+	SMTPServer  string `yaml:"smtp_server"`
+	SMTPPort    int    `yaml:"smtp_port"`
+	SenderLogin string `yaml:"sender_login"`
+	SenderPass  string `yaml:"sender_pass"`
+	Recipient   string `yaml:"recipient"`
+}
+
+func buildResponse(body string) *events.APIGatewayProxyResponse {
 	return &events.APIGatewayProxyResponse{
-		StatusCode:        200,
-		Headers:           map[string]string{"Content-Type": "text/plain"},
-		Body:              "Hello, World! ebat kopat",
-		IsBase64Encoded:   false,
-	}, nil
+		StatusCode:      200,
+		Headers:         map[string]string{"Content-Type": "text/plain"},
+		Body:            body,
+		IsBase64Encoded: false,
+	}
+}
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	// parse yaml config
+	yamlFile, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		return buildResponse(responseBadConfigFile), nil
+	}
+
+	conf := &config{}
+	err = yaml.Unmarshal(yamlFile, conf)
+	if err != nil {
+		return buildResponse(responseBadConfigContent), nil
+	}
+
+	// establishing a smtp connection
+	auth := smtp.PlainAuth("pireactor", conf.SenderLogin, conf.SenderPass, conf.SMTPServer)
+
+	// composing the message
+	to := []string{conf.Recipient}
+	msg := []byte("To: " + conf.Recipient + "\r\n" +
+		"Subject: " + ClientName + "\r\n\r\n" +
+		request.Body + "\r\n")
+
+	// sending it
+	err = smtp.SendMail(conf.SMTPServer+":"+strconv.Itoa(conf.SMTPPort), auth, conf.SenderLogin, to, msg)
+	if err != nil {
+		return buildResponse(responseSMTPError), nil
+	}
+
+	return buildResponse(responseOK), nil
 }
 
 func main() {
