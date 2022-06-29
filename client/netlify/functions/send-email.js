31ed727d6884a285edb7f6edeb4492f9d50cb563
unknown <ilia@e1off.dev>
2022-06-29 13:17:31 +0300

    feat services as tags

--- client/netlify/functions/send-email.js
+++ client/netlify/functions/send-email.js
@@ -1,6 +1,8 @@
 exports.handler = async function (event, context) {
+  console.log("event =>", event);
+  console.log("context =>", context);
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: event, context }),
   };
 }
