a7d83b8d4cfd20528a70a02e5fe91e07adcedc9a
unknown <ilia@e1off.dev>
2022-06-29 17:08:19 +0300

    feat services as tags

--- client/src/components/solid/TextInput/TextInput.tsx
+++ client/src/components/solid/TextInput/TextInput.tsx
@@ -2,13 +2,14 @@ import styles from "./TextInput.module.scss"
 
 interface ITextInputProps {
   label: string;
+  name?: string;
 }
 
 export function TextInput(props: ITextInputProps) {
   return (
     <label class={styles.label}>
       {props.label}
-      <input class={styles.input} type="text" />
+      <input name={props.label} class={styles.input} type="text" />
     </label>
   )
 }
